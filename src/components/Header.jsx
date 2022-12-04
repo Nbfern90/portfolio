12b7e7e6b75c2781b7efbe20f25236f619aa3d6e
Nick Fernandez <nbfern90@gmail.com>
2022-12-04 15:01:55 -0600

    Added Responsive Design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const clickHandler = () => setOpen(!open);
   return (
     <header className="header">
-      <div className="logo">
-        <Link to="/">Nicholas Fernandez</Link>
-      </div>
-      <ul>
+      <div className="logo">Nicholas Fernandez</div>
+      <ul className={open ? "nav-menu active" : "nav-menu"}>
         <a href="#about">
-          <li>About</li>
+          <li onClick={clickHandler}>About</li>
         </a>
         <a href="#technologies">
-          <li>Technologies</li>
+          <li onClick={clickHandler}>Technologies</li>
         </a>
         <a href="#projects">
-          <li>Projects</li>
+          <li onClick={clickHandler}>Projects</li>
         </a>
         <a href="#contact">
-          <li>Contact</li>
+          <li onClick={clickHandler}>Contact</li>
         </a>
       </ul>
+      <div className="hamburger" onClick={clickHandler}>
+        {open ? <FaTimes /> : <FaBars />}
+      </div>
     </header>
   );
 };
